04b6548f056f8d1bf0ed92077650df8d91540d13
Matt Puchyr <mattpuchyr@gmail.com>
2021-01-29 14:07:03 -0600

    Create form and onNameChange function in components/NewGame.js

--- src/components/NewGame.js
+++ src/components/NewGame.js
@@ -1,8 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { v4 as uuid } from 'uuid';
 
 const NewGame = () => {
+    const [currentGame, setCurrentGame] = useState({id: uuid(), players: []})
+    const [playerToAdd, setPlayerToAdd] = useState({name: '', score: 0})
+    
+    const onNameChange = (e) => {
+        setPlayerToAdd({
+            ...playerToAdd,
+            name: e.target.value
+        })
+    }
+
     return (
-        <h1>New Game Placeholder</h1>
+        <div>
+            <h1>New Game Placeholder</h1>
+            <p>{currentGame.id}</p>
+            <h2>Add Player</h2>
+            <form>
+                <input onChange={onNameChange} type="text" name="player_name" placeholder="Name" value={playerToAdd.name} />
+            </form>
+        </div>
     )
 }
 
