ee52f97ddc1558b28f7059b9eb19749af31cd2f8
Matt Puchyr <mattpuchyr@gmail.com>
2021-01-29 14:29:57 -0600

    Create colorSelection and onColorChange functions in components/NewGame.js

--- src/components/NewGame.js
+++ src/components/NewGame.js
@@ -3,13 +3,23 @@ import { v4 as uuid } from 'uuid';
 
 const NewGame = () => {
     const blankPlayerTemplate = {name: '', score: 0}
+    const colors = ['black', 'blue', 'green', 'pink', 'red', 'yellow']
+    
     const [currentGame, setCurrentGame] = useState({id: uuid(), players: []})
     const [playerToAdd, setPlayerToAdd] = useState(blankPlayerTemplate)
+    const [colorOptions, setColorOptions] = useState(colors)
     
     const onNameChange = (e) => {
         setPlayerToAdd({
             ...playerToAdd,
             name: e.target.value
+        })
+    }
+
+    const onColorChange = (e) => {
+        setPlayerToAdd({
+            ...playerToAdd,
+            color: e.target.value
         })
     }
 
@@ -22,6 +32,12 @@ const NewGame = () => {
         setPlayerToAdd(blankPlayerTemplate)
     }
 
+    const colorSelection = () => {
+        return (
+            colorOptions.map(color => <option key={color} value={color}>{color.charAt(0).toUpperCase() + color.slice(1)}</option>)
+        )
+    }
+
     return (
         <div>
             <h1>New Game Placeholder</h1>
@@ -29,6 +45,9 @@ const NewGame = () => {
             <h2>Add Player</h2>
             <form onSubmit={onAddPlayer}>
                 <input onChange={onNameChange} type="text" name="player_name" placeholder="Name" value={playerToAdd.name} />
+                <select onChange={onColorChange}>
+                    {colorSelection()}
+                </select>
                 <button>Add Player</button>
             </form>
         </div>
