e266d0f679079744c1c9a415c8975c6f75377d66
Matt Puchyr <mattpuchyr@gmail.com>
2021-01-29 14:50:00 -0600

    Prevent user from adding player without name and color in onAddPlayer in components/NewGame.js

--- src/components/NewGame.js
+++ src/components/NewGame.js
@@ -25,11 +25,16 @@ const NewGame = () => {
 
     const onAddPlayer = (e) => {
         e.preventDefault()
-        setCurrentGame({
-            ...currentGame,
-            players: [...currentGame.players, playerToAdd]
-        })
-        setPlayerToAdd(blankPlayerTemplate)
+        if (!!playerToAdd.name && !!playerToAdd.color) {
+            setCurrentGame({
+                ...currentGame,
+                players: [...currentGame.players, playerToAdd]
+            })
+            setColorOptions(colorOptions.filter(color => color !== playerToAdd.color))
+            setPlayerToAdd(blankPlayerTemplate)
+        } else {
+            alert('Player must have both name and color!')
+        }
     }
 
     const colorSelection = () => {
@@ -43,9 +48,13 @@ const NewGame = () => {
             <h1>New Game Placeholder</h1>
             <p>{currentGame.id}</p>
             <h2>Add Player</h2>
+            <ul>
+                {currentGame.players.map(player => <li key={player.name}>{player.name} {player.color}</li>)}
+            </ul>
             <form onSubmit={onAddPlayer}>
                 <input onChange={onNameChange} type="text" name="player_name" placeholder="Name" value={playerToAdd.name} />
                 <select onChange={onColorChange}>
+                    <option value="" required>Color</option>
                     {colorSelection()}
                 </select>
                 <button>Add Player</button>
