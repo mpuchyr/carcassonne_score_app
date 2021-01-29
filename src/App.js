4bbaaff47e84e810de9221803c2ba02e60361205
Matt Puchyr <mattpuchyr@gmail.com>
2021-01-29 15:35:15 -0600

    Create context/game-context and import it into App.js

--- src/App.js
+++ src/App.js
@@ -3,19 +3,20 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import History from './components/History';
 import NewGame from './components/NewGame';
 import Player from './components/Player';
-import System from './components/System'
+import System from './components/System';
+import GameContext from './context/game-context';
 
 function App() {
   return (
-    <Router>
-      <h1>Placeholder</h1>
-      <Switch>
-        <Route exact path="/history" component={History} />
-        <Route exact path="/newgame" component={NewGame} />
-        <Route exact path="/players/:id" component={Player} />
-        <Route exact path="/system" component={System} />
-      </Switch>
-    </Router>
+      <Router>
+        <h1>Placeholder</h1>
+        <Switch>
+          <Route exact path="/history" component={History} />
+          <Route exact path="/newgame" component={NewGame} />
+          <Route exact path="/players/:id" component={Player} />
+          <Route exact path="/system" component={System} />
+        </Switch>
+      </Router>
   );
 }
 
