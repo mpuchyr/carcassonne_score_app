d59549b9c8101ebcfb4e98668f14634d08b18e9d
Matt Puchyr <mattpuchyr@gmail.com>
2021-01-30 14:07:51 -0600

    Create basic form in components/features/City.js

--- src/components/features/City.js
+++ src/components/features/City.js
@@ -1,9 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const City = () => {
+    const [score, setScore] = useState(0)
+    
     return (
         <div>
-            <h1>City Placeholder</h1>
+            <h1>City</h1>
+            <form>
+                <label>Number of City Tiles: </label>
+                <input type="number" min="0" />
+                <input type="checkbox" />
+                <label>City has a cathedral</label>
+                <input type="checkbox" />
+                <label>This is the end game</label>
+                <input type="checkbox" />
+                <label>Feature is shared</label>
+                <button>Add</button>
+            </form>
         </div>
     )
 }
