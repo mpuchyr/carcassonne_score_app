dc7f72f7d832b15bf9e4a9822f83cb1e5f2afff8
Matt Puchyr <mattpuchyr@gmail.com>
2021-01-30 14:17:00 -0600

    Create basic form in components/features/Farm.js and fix bug in components/AddFeature that called City instead of Farm

--- src/components/AddFeature.js
+++ src/components/AddFeature.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import Barn from './features/Barn';
 import City from './features/City';
-import Farm from './features/City';
+import Farm from './features/Farm';
 import Manual from './features/Manual';
 import Monastary from './features/Monastary';
 import Road from './features/Road';
 
 const AddFeature = (props) => {
-    console.log(props.match.params.featurename)
+    const currentFeature = props.match.params.featurename
     
-    const featureToShow = () => {
+    const featureToShow = (feature) => {
         switch(props.match.params.featurename) {
             case 'barn':
                 return <Barn />
@@ -30,7 +30,7 @@ const AddFeature = (props) => {
         <div>
             <h1>Add Feature Placeholder</h1>
             <p>{props.match.params.featurename}</p>
-            {featureToShow()}
+            {featureToShow(currentFeature)}
         </div>
     )
 }
--- src/components/features/Farm.js
+++ src/components/features/Farm.js
@@ -1,9 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Farm = () => {
+    const [score, setScore] = useState(0)
+    
     return (
         <div>
-            <h1>Farm Placeholder</h1>
+            <h1>Farm</h1>
+            <form>
+                <label>Cities: </label>
+                <input type="number" min="0" />
+                <label>Castles: </label>
+                <input type="number" min="0" />
+                <input type="checkbox" />
+                <label>Feature is shared</label>
+                <p>Total: {score}</p>
+                <button>Add</button>
+            </form>
         </div>
     )
 }
