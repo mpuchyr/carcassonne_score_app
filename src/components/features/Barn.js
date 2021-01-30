f39287a395cb60f3b2d182cb9a381297ea7e6173
Matt Puchyr <mattpuchyr@gmail.com>
2021-01-30 14:02:30 -0600

    Create basic form in components/features/Barn.js

--- src/components/features/Barn.js
+++ src/components/features/Barn.js
@@ -1,9 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Barn = () => {
+    const [score, setScore] = useState(0)
+    const [shared, setShared] = useState(false)
+
+
     return (
         <div>
-            <h1>Barn Placeholder</h1>
+            <h1>Barn</h1>
+            <form>
+                <label>Cities: </label>
+                <input type="number" min="0" />
+                <label>Castles: </label>
+                <input type="number" min="0" />
+                <input type="checkbox" />
+                <label>Feature is shared</label>
+                <p>Total: {score}</p>
+                <button>Add</button>
+            </form>
         </div>
     )
 }
