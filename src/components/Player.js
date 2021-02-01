5e2f99681eeee31bf5edceb9e14ff579f0022dad
Matt Puchyr <mattpuchyr@gmail.com>
2021-02-01 15:14:49 -0600

    Add NavLinks to multiple features in components/Player.js

--- src/components/Player.js
+++ src/components/Player.js
@@ -5,13 +5,24 @@ import { NavLink } from 'react-router-dom';
 const Player = (props) => {
     const { game } = useContext(GameContext)
 
-    const toBarn = `/players/${props.match.params.id}/barn`
-    const destination = `/players/${props.match.params.id}/manual`
+    const id = props.match.params.id
+    const locationPartial = `/players/${id}`
+
+    const toBarn = `${locationPartial}/barn`
+    const toCity = `${locationPartial}/city`
+    const toFarm = `${locationPartial}/farm`
+    const toMonastary = `${locationPartial}/monastary`
+    const toRoad = `${locationPartial}/road`
+    const destination = `${locationPartial}/manual`
     
     return (
         <div>
             <h1>Player #{props.match.params.id} Placeholder</h1>
             <NavLink to={toBarn}>Barn</NavLink>
+            <NavLink to={toCity}>City</NavLink>
+            <NavLink to={toFarm}>Farm</NavLink>
+            <NavLink to={toMonastary}>Monastary</NavLink>
+            <NavLink to={toRoad}>Road</NavLink>
             <NavLink to={destination}>Manual Point Entry</NavLink>
             <button onClick={() => console.log(game)}>Click Me</button>
         </div>
