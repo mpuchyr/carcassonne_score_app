e93886196427870dde9f804f48f821e95cf3533a
Matt Puchyr <mattpuchyr@gmail.com>
2021-02-01 16:05:56 -0600

    Create form in components/features/Road.js

--- src/components/features/Road.js
+++ src/components/features/Road.js
@@ -7,6 +7,14 @@ const Road = () => {
     return (
         <div>
             <h1>Road Score Placeholder</h1>
+            <form>
+                <label>Number of Road Tiles: </label>
+                <input type="number" min="0" />
+                <input type="checkbox" />
+                <label>Road has an Inn</label>
+                <input type="checkbox" />
+                <label>This feature is shared</label>
+            </form>
         </div>
     )
 }
