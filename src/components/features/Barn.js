14f0fec0272c819fb086ba2c074213024ee31fab
Matt Puchyr <mattpuchyr@gmail.com>
2021-02-01 14:54:17 -0600

    Create addCastlePoints and addCityPoints functions in components/features/Barn.js

--- src/components/features/Barn.js
+++ src/components/features/Barn.js
@@ -3,6 +3,16 @@ import React, { useState } from 'react';
 const Barn = () => {
     const [score, setScore] = useState(0)
     const [shared, setShared] = useState(false)
+    const [castlePoints, setCastlePoints] = useState(0)
+    const [cityPoints, setCityPoints] = useState(0)
+
+    const addCastlePoints = (e) => {
+        setCastlePoints(e.target.value * 5)
+    }
+
+    const addCityPoints = (e) => {
+        setCityPoints(e.target.value * 4)
+    }
 
 
     return (
@@ -10,12 +20,12 @@ const Barn = () => {
             <h1>Barn</h1>
             <form>
                 <label>Cities: </label>
-                <input type="number" min="0" />
+                <input type="number" min="0" onChange={addCityPoints}/>
                 <label>Castles: </label>
-                <input type="number" min="0" />
+                <input type="number" min="0" onChange={addCastlePoints} />
                 <input type="checkbox" />
                 <label>Feature is shared</label>
-                <p>Total: {score}</p>
+                <p>Total: {cityPoints + castlePoints}</p>
                 <button>Add</button>
             </form>
         </div>
