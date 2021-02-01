5424a80e5a39c36b8962ff1f164bddf47b4c90f5
Matt Puchyr <mattpuchyr@gmail.com>
2021-02-01 16:13:05 -0600

    Create multiple functions in components/features/Road.js to allow user to update a player's score

--- src/components/features/Road.js
+++ src/components/features/Road.js
@@ -1,19 +1,34 @@
-import React, { useState } from 'react';
+import React, { useContext, useState } from 'react';
+import { addPoints } from '../../actions/actions';
+import GameContext from '../../context/game-context'; 
 
-const Road = () => {
+const Road = ({ playerId, history }) => {
     const [score, setScore] = useState(0)
     const [hasInn, setHasInn] = useState(false)
 
+    const { game, dispatch} = useContext(GameContext)
+
+    const addRoads = (e) => {
+        hasInn ? setScore(parseInt(e.target.value) * 2) : setScore(parseInt(e.target.value))
+    }
+
+    const onSubmit = (e) => {
+        e.preventDefault()
+        dispatch(addPoints(playerId, score))
+        history.push('/')
+    }
     return (
         <div>
             <h1>Road Score Placeholder</h1>
-            <form>
+            <form onSubmit={onSubmit}>
                 <label>Number of Road Tiles: </label>
-                <input type="number" min="0" />
+                <input type="number" min="0" onChange={addRoads}/>
                 <input type="checkbox" />
                 <label>Road has an Inn</label>
                 <input type="checkbox" />
                 <label>This feature is shared</label>
+                <p>Total: {score}</p>
+                <button>Add</button>
             </form>
         </div>
     )
