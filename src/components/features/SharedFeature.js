6c0bdbda76e0fb9b94525f9f0dc3965f85b6486e
Matt Puchyr <mattpuchyr@gmail.com>
2021-02-02 14:25:36 -0600

    Import GameContext into components/features/SharedFeature.js

--- src/components/features/SharedFeature.js
+++ src/components/features/SharedFeature.js
@@ -1,6 +1,9 @@
 import React, { useContext } from 'react';
+import GameContext from '../../context/game-context';
 
 const SharedFeature = () => {
+    const { game, dispatch} = useContext(GameContext)
+    
     return (
         <div>
             <h1>Shared Feature Placeholder</h1>
