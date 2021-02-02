9b3a78bac69283002630d96d8808515ef0b7592f
Matt Puchyr <mattpuchyr@gmail.com>
2021-02-02 14:24:13 -0600

    Create components/features/SharedFeature.js

---
+++ src/components/features/SharedFeature.js
@@ -0,0 +1,11 @@
+import React, { useContext } from 'react';
+
+const SharedFeature = () => {
+    return (
+        <div>
+            <h1>Shared Feature Placeholder</h1>
+        </div>
+    )
+}
+
+export default SharedFeature
