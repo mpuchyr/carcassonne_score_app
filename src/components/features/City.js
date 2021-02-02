0c0694661b8558f281f6d21eb823404b87e006e0
Matt Puchyr <mattpuchyr@gmail.com>
2021-02-02 15:40:33 -0600

    Create hasCathedral and endGame variables in components/City.js

--- src/components/features/City.js
+++ src/components/features/City.js
@@ -2,16 +2,26 @@ import React, { useState } from 'react';
 
 const City = () => {
     const [score, setScore] = useState(0)
+    const [hasCathedral, setHasCathedral] = useState(false)
+    const [endGame, setEndGame] = useState(false)
+
+    const isEndGame = () => {
+        setEndGame(!endGame)
+    }
+
+    const cityHasCathedral = () => {
+        setHasCathedral(!hasCathedral)
+    }
     
     return (
         <div>
             <h1>City</h1>
             <form>
-                <label>Number of City Tiles: </label>
+                <label>Number of City Tiles, including Shields: </label>
                 <input type="number" min="0" />
-                <input type="checkbox" />
+                <input type="checkbox" onChange={cityHasCathedral}/>
                 <label>City has a cathedral</label>
-                <input type="checkbox" />
+                <input type="checkbox" onChange={isEndGame}/>
                 <label>This is the end game</label>
                 <input type="checkbox" />
                 <label>Feature is shared</label>
