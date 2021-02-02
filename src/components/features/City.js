8fd5ad724956731a9fc099bfa19457cf8a1ff142
Matt Puchyr <mattpuchyr@gmail.com>
2021-02-02 15:48:19 -0600

    Create multiple functions in components/features/City.js

--- src/components/features/City.js
+++ src/components/features/City.js
@@ -12,19 +12,32 @@ const City = () => {
     const cityHasCathedral = () => {
         setHasCathedral(!hasCathedral)
     }
+
+    const onChange = (e) => {
+        if (hasCathedral && endGame) {
+            setScore(0)
+        } else if (hasCathedral) {
+            setScore(e.target.value * 3)
+        } else if (endGame) {
+            setScore(e.target.value)
+        } else {
+            setScore(e.target.value * 2)
+        }
+    }
     
     return (
         <div>
             <h1>City</h1>
             <form>
                 <label>Number of City Tiles, including Shields: </label>
-                <input type="number" min="0" />
+                <input type="number" min="0" onChange={onChange}/>
                 <input type="checkbox" onChange={cityHasCathedral}/>
                 <label>City has a cathedral</label>
                 <input type="checkbox" onChange={isEndGame}/>
                 <label>This is the end game</label>
                 <input type="checkbox" />
                 <label>Feature is shared</label>
+                <p>Total: {score}</p>
                 <button>Add</button>
             </form>
         </div>
