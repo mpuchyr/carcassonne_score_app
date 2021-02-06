0f9fa2400b69d83f1fc4a55505dc19c3c5221c04
Matt Puchyr <mattpuchyr@gmail.com>
2021-02-06 14:41:47 -0600

    Create showSavedGames function in components/LoadGame.js

--- src/components/LoadGame.js
+++ src/components/LoadGame.js
@@ -1,9 +1,29 @@
 import React from 'react';
+import moment from 'moment';
 
 const LoadGame = () => {
+    const savedGames = JSON.parse(localStorage.getItem('savedGames'))
+
+    const showSavedGames = () => {
+        return savedGames.map(game => {
+            return (
+                <div key={game.id}>
+                    <h2>{moment(game.gameDate).format('MMMM Do YYYY, h:mm a')}</h2>
+                    <ul>
+                        {game.players.map(player => {
+                            return <li key={player.id}>{player.name}</li>
+                        })}
+                    </ul>
+                    <button>Load</button>
+                </div>
+            )
+        })
+    }
+    
     return (
         <div>
             <h1>Load Game Placeholder</h1>
+            {showSavedGames()}
         </div>
     )
 }
