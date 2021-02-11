c5a0e42036c4bd4ce2e7299f6cc77b744b4d409d
Matt Puchyr <mattpuchyr@gmail.com>
2021-02-11 14:38:34 -0600

    Remove buttons on components/Main.js and only show New Game button if there are no players

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,7 +1,6 @@
 import React, { useContext } from 'react';
 import moment from 'moment';
 import GameContext from '../context/game-context';
-import { saveGame } from '../actions/actions';
 
 const Main = (props) => {
     const { game, dispatch } = useContext(GameContext)
@@ -28,12 +27,11 @@ const Main = (props) => {
         <div>
             <h1>{moment(game.gameDate).format('MMMM Do YYYY, h:mm:ss a')}</h1>
             {game.players && showAllPlayers()}
-            <button onClick={() => props.history.push('/newgame')}>New Game</button>
+            {game.players.length === 0 && <button onClick={() => props.history.push('/newgame')}>New Game</button>}
             <button onClick={() => props.history.push('/history')}>History</button>
-            <button onClick={() => props.history.push('/loadgame')}>Load Game</button>
             <button onClick={() => props.history.push('/system')}>System</button>
             <button onClick={() => console.log(game)}>Click Me</button>
-            <button onClick={() => dispatch(saveGame())}>Save Game</button>
+
         </div>
     )
 }
