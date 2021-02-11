68f312447251ddc2449e94b0aed877d0d5bf8dda
Matt Puchyr <mattpuchyr@gmail.com>
2021-02-11 14:33:32 -0600

    Add multiple buttons and onGameSave function  in components/System.js

--- src/components/System.js
+++ src/components/System.js
@@ -1,11 +1,21 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import GameContext from '../context/game-context';
+import { saveGame } from '../actions/actions';
 
 
 const System = ({ history }) => {
+    const { dispatch } = useContext(GameContext)
+
+    const onGameSave = () => {
+        dispatch(saveGame())
+        alert('Game Saved')
+    }
+
     return (
         <div>
             <h1>System Placeholder</h1>
             <button onClick={() => history.push('/newgame')}>New Game</button>
+            <button onClick={onGameSave}>Save Game</button>
             <button onClick={() => history.push('/loadgame')}>Load Game</button>
             <button onClick={() => history.goBack()}>Back</button>
         </div>
