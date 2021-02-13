9c2e56ae95f6349cef85092c5cb3bd45dddfae5d
Matt Puchyr <mattpuchyr@gmail.com>
2021-02-13 13:12:12 -0600

    Import Menu into components/MainHeader.js

--- src/components/MainHeader.js
+++ src/components/MainHeader.js
@@ -1,13 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { NavLink } from 'react-router-dom';
+import { slide as Menu } from 'react-burger-menu';
 
 const MainHeader = () => {
+    const [menuIsOpen, setMenuIsOpen] = useState(false)
+
+    const openMenu = () => {
+        setMenuIsOpen(true)
+    }
+
+    const closeMenu = () => {
+        setMenuIsOpen(false)
+    }
+
     return (
         <div>
+            <button onClick={openMenu}>Menu</button>
+            <Menu isOpen={menuIsOpen} width={300}>
+                <button><NavLink to="/newgame">New Game</NavLink></button>
+                <button><NavLink to="/loadgame">Load Game</NavLink></button>
+                <button><NavLink to="/savedplayers">All Saved Players</NavLink></button>
+            </Menu>
             <h1>Carcassonne</h1>
-            <button>
-                <NavLink to="/system">System</NavLink>
-            </button>
         </div>
     )
 }
