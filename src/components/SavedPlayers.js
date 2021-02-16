6149f27c551f48d3ad561b2a362676d9af29d79d
Matt Puchyr <mattpuchyr@gmail.com>
2021-02-16 14:43:22 -0600

    Create showAllSavedPlayers function in components/SavedPlayers.js

--- src/components/SavedPlayers.js
+++ src/components/SavedPlayers.js
@@ -1,8 +1,25 @@
 import React from 'react';
 
 const SavedPlayers = ({ history }) => {
+    const allSavedPlayers = JSON.parse(localStorage.getItem('players'))
+
+    const showAllSavedPlayers = () => {
+        if (allSavedPlayers.length > 0) {
+            return allSavedPlayers.map(player => {
+                return (
+                    <li key={player.id}>
+                        {player.name}
+                        <button>X</button>
+                    </li>)
+            })
+        }
+    }
+
     return (
         <div>
+            <ul>
+                {showAllSavedPlayers()}
+            </ul>
             <button onClick={() => history.goBack()}>Back</button>
         </div>
     )
