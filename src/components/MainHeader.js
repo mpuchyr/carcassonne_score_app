f77b74e36cda5e7c3d22fefccc959fba96e0afd3
Matt Puchyr <mattpuchyr@gmail.com>
2021-02-26 13:58:19 -0600

    Create handleClickOutside function to hide menu in components/MainHeader.js

--- src/components/MainHeader.js
+++ src/components/MainHeader.js
@@ -1,18 +1,33 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 
 const MainHeader = () => {
     const [menuIsVisible, setMenuIsVisible] = useState(false)
+
+    const reference = React.createRef()
+
+    const handleClickOutside = e => {
+        if (reference.current && !reference.current.contains(e.target)) {
+            console.log('this is outside the area')
+            setMenuIsVisible(false)
+        }
+    }
+
+    useEffect(() => {
+        document.addEventListener('mousedown', handleClickOutside)
+
+        return (() => document.removeEventListener('mousedown', handleClickOutside))
+    })
     
     return (
         <div>
             <button onClick={() => setMenuIsVisible(!menuIsVisible)}>Menu</button>
             <h1 className="main-header-title">Carcassonne</h1> 
-            <div id={menuIsVisible ? "menu-visible" : "menu-invisible"}>
+            {menuIsVisible && <div className="dropdown" ref={reference}>
                 <a href="/"><button>Home</button></a>
                 <a href="/newgame"><button>New Game</button></a>
                 <a href="/loadgame"><button>Load Game</button></a>
                 <a href="/savedplayers"><button>All Saved Players</button></a>
-            </div>
+            </div>}
         </div>
     )
 }
