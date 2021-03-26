fbafd15523074e7e8e9b6486d3a1319e6809d759
Matt Puchyr <mattpuchyr@gmail.com>
2021-03-26 15:43:56 -0500

    Update components/features/Road.js to properly display total

--- src/components/features/Road.js
+++ src/components/features/Road.js
@@ -1,23 +1,36 @@
-import React, { useContext, useState } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import { addPoints, saveGame } from '../../actions/actions';
 import GameContext from '../../context/game-context'; 
 import ModalContext from '../../context/modal-context';
 import SharedFeatureModal from './SharedFeatureModal';
 
 const Road = ({ playerId, history, currentFeature }) => {
-    const [score, setScore] = useState(0)
     const [hasInn, setHasInn] = useState(false)
     const [isShared, setIsShared] = useState(false)
+    const [points, setPoints] = useState(0)
+    const [total, setTotal] = useState(0)
 
     const { game, dispatch} = useContext(GameContext)
     const { openModal } = useContext(ModalContext)
+
+    useEffect(() => {
+        changeTotal()
+    })
+
+    const changeTotal = () => {
+        if (hasInn) {
+            setTotal(points * 2)
+        } else {
+            setTotal(points)
+        }
+    }
 
     const featureIsShared = () => {
         setIsShared(!isShared)
     }
 
     const addRoads = (e) => {
-        hasInn ? setScore(parseInt(e.target.value) * 2) : setScore(parseInt(e.target.value))
+        setPoints(e.target.value)
     }
 
     const roadHasAnInn = () => {
@@ -27,7 +40,7 @@ const Road = ({ playerId, history, currentFeature }) => {
     const onSubmit = (e) => {
         e.preventDefault()
         if (!isShared) {
-            dispatch(addPoints(playerId, score, currentFeature))
+            dispatch(addPoints(playerId, total, currentFeature))
             dispatch(saveGame())
             history.push('/')
         } else {
@@ -44,14 +57,14 @@ const Road = ({ playerId, history, currentFeature }) => {
                 <label>Road has an Inn</label>
                 <input type="checkbox" onChange={featureIsShared}/>
                 <label>Feature is shared</label>
-                <p>Total: {score}</p>
+                <p>Total: {total}</p>
                 <button>Add</button>
             </form>
             <SharedFeatureModal 
                 playerId={playerId}
                 history={history}
                 featureName={currentFeature}
-                score={score}
+                score={total}
             />
         </div>
     )
